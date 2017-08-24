7b91016825b3409a361f76893946f08771424e90
Anthony Laude <30366704+Nykun@users.noreply.github.com>
2017-08-24 13:01:37 +1100

    Add files via upload

--- Quizz/vues/jouer.php
+++ Quizz/vues/jouer.php
@@ -1,18 +1,72 @@
 <!doctype html>
 <html lang="fr">
-
 	<head>
-		<title></title>
-		<meta name="Description" content=""/>
+		<!-- Remplir le titre -->
+		<title>Jouer</title>
+		<!-- Remplir la description -->
+		<meta name="Description" content="Page jouer, acces aux modes de jeu"/>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
-		<link rel="Stylesheet" href="vue/polices.css" type="text/css">
-		<link rel="Stylesheet" href="vue/layout.css" type="text/css">
+		<link rel="Stylesheet" href="../CSS/polices.css" type="text/css">
+		<link rel="Stylesheet" href="../CSS/layout.css" type="text/css">
+		<link rel="Stylesheet" href="../CSS/bootstrap.min.css" type="text/css">
+		<script src="../js/jquery-1.11.2.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 		<!-- Remplir avec le bon css -->
-		<link rel="Stylesheet" href="vue/.css" type="text/css"> 
-		<link rel="Stylesheet" href="vue/bootstrap.min.css" type="text/css">
+		<link rel="Stylesheet" href="../CSS/jouer.css" type="text/css">
 	</head>
 
-	<body class="accueil">
+	<body class="container-fluid">
+		<div class="row">
+			<a href="#">
+				<div class="col-md-2 text-center">
+					<h1 class="mode">
+						D<br/>E<br/>F<br/>E<br/>N<br/>S<br/>E
+					</h1>		
+				</div>
+			</a>
 
+			<a href="#">
+				<div class="col-md-2 text-center">
+					<h1 class="mode">
+						A<br/>T<br/>T<br/>A<br/>Q<br/>U<br/>E
+					</h1>		
+				</div>
+			</a>
+
+			<a href="#">
+				<div class="col-md-2 text-center">
+					<h1 class="mode">
+						C<br/>A<br/>M<br/>P<br/>A<br/>G<br/>N<br/>E
+					</h1>		
+				</div>
+			</a>
+
+			<a href="#">
+				<div class="col-md-2 text-center">
+					<h1 class="mode">
+						A<br/>R<br/>E<br/>N<br/>E
+					</h1>		
+				</div>
+			</a>
+
+			<a href="#">
+				<div class="col-md-2 text-center">
+					<h1 class="mode">
+						G<br/>v<br/>G
+					</h1>
+				</div>
+			</a>
+
+			<a href="#">
+				<div class="col-md-2 text-center">
+					<h1 class="mode">
+						E<br/>V<br/>E<br/>N<br/>T
+					</h1>
+				</div>
+			</a>
+		</div>
+
+		<?php require 'layout.inc.php'; ?>
 	</body>
 </html>
