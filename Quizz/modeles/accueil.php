b9cd85f51afe93a549f40f0aba66626a8b0c0d8d
Anthony Laude <30366704+Nykun@users.noreply.github.com>
2017-09-04 15:08:29 +1100

    Add files via upload

--- Quizz/modeles/accueil.php
+++ Quizz/modeles/accueil.php
@@ -1,10 +1,5 @@
-<?php 
+<?php
 
-	require 'modeles/accueil.php';
 
-	include 'vues/creer_C.php';
-	include 'vues/inc.menu.php';
-	include 'vues/inc.flex.php';
-	include 'vues/inc.layout.php';
 
 ?>
--- Quizz/modeles/jouer.php
+++ Quizz/modeles/jouer.php
@@ -1,9 +1,5 @@
 <?php 
 
-	require 'modeles/jouer.php';
 
-	include 'vues/jouer.php';
-	include 'vues/inc.menu.php';
-	include 'vues/inc.layout.php';
 
 ?>
