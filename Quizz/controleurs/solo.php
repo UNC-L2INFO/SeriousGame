64d401835d99a3fca37f9c8842d76dcf25c82880
Anthony Laude <30366704+Nykun@users.noreply.github.com>
2017-09-14 19:10:56 +1100

    Add files via upload

---
+++ Quizz/controleurs/solo.php
@@ -0,0 +1,10 @@
+<?php 
+
+	require 'modeles/solo.php';
+
+	$Sujet = $_POST['Sujet'];
+
+	include 'vues/solo.php';
+	include 'vues/inc.layout.php';
+
+?>
---
+++ Quizz/controleurs/solo_choix_sujet.php
@@ -0,0 +1,10 @@
+<?php 
+
+	require 'modeles/solo_choix_sujet.php';
+
+	include 'vues/solo_choix_sujet.php';
+	include 'vues/inc.menu.php';
+	include 'vues/inc.flex.php';
+	include 'vues/inc.layout.php';
+
+?>
