f4b365eaad39cf6a520af57007a1d5831abf91ad
Anthony Laude <30366704+Nykun@users.noreply.github.com>
2017-09-14 19:16:18 +1100

    mauvaise vue

--- Quizz/controleurs/accueil.php
+++ Quizz/controleurs/accueil.php
@@ -2,7 +2,7 @@
 
 	require 'modeles/accueil.php';
 
-	include 'vues/creer_C.php';
+	include 'vues/accueil.php';
 	include 'vues/inc.menu.php';
 	include 'vues/inc.flex.php';
 	include 'vues/inc.layout.php';
