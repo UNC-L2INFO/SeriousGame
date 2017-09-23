d9539ad8b39a165b046703749aa3a4c4a5ae7786
Jimmy Avae <30336781+zJimmyz@users.noreply.github.com>
2017-09-23 21:51:19 +1100

    Sa date

--- Quizz/vues/connecter.php
+++ Quizz/vues/connecter.php
@@ -1,18 +1,22 @@
-<!doctype html>
-<html lang="fr">
+<?php $title = 'Connecter'; ?>
+<?php $desc = 'Page d\'accueil affichant les news, le top 10 du classement, les patch notes et d\'autres informations'; ?>
 
-	<head>
-		<title></title>
-		<meta name="Description" content=""/>
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
-		<link rel="Stylesheet" href="vue/polices.css" type="text/css">
-		<link rel="Stylesheet" href="vue/layout.css" type="text/css">
-		<!-- Remplir avec le bon css -->
-		<link rel="Stylesheet" href="vue/.css" type="text/css"> 
-		<link rel="Stylesheet" href="vue/bootstrap.min.css" type="text/css">
-	</head>
-
-	<body class="accueil">
-
-	</body>
-</html>
+<?php ob_start(); ?>
+   <!-- Page Content -->
+    <div class="flex-body">
+    <a id="debut"> </a>
+        <div class="main-box-medium">
+            <section class="l">
+                <form method="post" action="?page=compte_perso">
+                        <label for="login"> Nom de Compte: </label>
+                            <input type="text" name="login" id="login" placeholder="Login" maxlength="12" />
+                            <br />
+                        <label for="idpassword"> Votre mot de passe: </label>
+                    <input type="password" name="password" id="password" placeholder="Password" maxlength="12" />
+                    <input type="submit" value="Envoyer">
+                </form>
+                <a href="">Créer un compte</a>
+             </section>
+         </div>
+    </div>
+<?php $content = ob_get_clean(); ?>
